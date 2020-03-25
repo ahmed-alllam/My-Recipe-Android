5b04874c89832b46dda9ae57f38353d83f010342
Ahmed Emad <ahmed39652003@gmail.com>
2020-03-25 19:53:12 +0200

    finished login layout -- add user model -- added splash activity -- added icons

---
+++ app/src/main/java/com/koshkie/myrecipeapp/ui/Activities/LoginActivity.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Code Written and Tested by Ahmed Emad in 25/03/20 19:53
+ */
+
+package com.koshkie.myrecipeapp.ui.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.koshkie.myrecipeapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
