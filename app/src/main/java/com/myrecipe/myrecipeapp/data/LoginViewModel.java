24a7fc28cdb31249b55b26143b5093462e662c8b
Ahmed Emad <ahmed39652003@gmail.com>
2020-04-04 00:49:30 +0200

    added error checking and validation -- bug fixes

--- app/src/main/java/com/myrecipe/myrecipeapp/data/LoginViewModel.java
+++ app/src/main/java/com/myrecipe/myrecipeapp/data/LoginViewModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Code Written and Tested by Ahmed Emad in 03/04/20 21:32
+ * Copyright (c) Code Written and Tested by Ahmed Emad in 04/04/20 00:49
  */
 
 package com.myrecipe.myrecipeapp.data;
@@ -7,6 +7,7 @@ package com.myrecipe.myrecipeapp.data;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.myrecipe.myrecipeapp.R;
 import com.myrecipe.myrecipeapp.models.UserLoginModel;
 
 import retrofit2.Call;
@@ -23,12 +24,15 @@ public class LoginViewModel extends ViewModel {
         loginAPIInterface.loginUser(email, password).enqueue(new Callback<UserLoginModel>() {
             @Override
             public void onResponse(Call<UserLoginModel> call, Response<UserLoginModel> response) {
-                userLoginModel.setValue(response.body());
+                if (response.code() == 400)
+                    error.setValue(R.string.wrong_email_or_password);
+                else
+                    userLoginModel.setValue(response.body());
             }
 
             @Override
             public void onFailure(Call<UserLoginModel> call, Throwable t) {
-                // todo
+                error.setValue(R.string.network_error);
             }
         });
     }
--- app/src/main/java/com/myrecipe/myrecipeapp/ui/Activities/LoginActivity.java
+++ app/src/main/java/com/myrecipe/myrecipeapp/ui/Activities/LoginActivity.java
@@ -1,12 +1,14 @@
 /*
- * Copyright (c) Code Written and Tested by Ahmed Emad in 03/04/20 21:32
+ * Copyright (c) Code Written and Tested by Ahmed Emad in 04/04/20 00:49
  */
 
 package com.myrecipe.myrecipeapp.ui.Activities;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -15,29 +17,46 @@ import com.myrecipe.myrecipeapp.R;
 import com.myrecipe.myrecipeapp.data.LoginViewModel;
 
 public class LoginActivity extends AppCompatActivity {
+    LoginViewModel loginViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+
+        loginViewModel.userLoginModel.observe(this, userLoginModel -> {
+            // todo
+            System.out.println("observer 1");
+            findViewById(R.id.progressBar).setVisibility(View.INVISIBLE);
+        });
+        loginViewModel.error.observe(this, error -> {
+            System.out.println("observer 2");
+            ((TextView) findViewById(R.id.error)).setText(error);
+            findViewById(R.id.progressBar).setVisibility(View.INVISIBLE);
+        });
     }
 
     public void login(View view) {
         String email = ((EditText) findViewById(R.id.emailField)).getText().toString();
         String password = ((EditText) findViewById(R.id.passwordField)).getText().toString();
 
-        // todo add validation
+        findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
 
-        LoginViewModel loginViewModel = new ViewModelProvider(this)
-                .get(LoginViewModel.class);
+        if (!validEmail(email) || !validPassword(password)) {
+            ((TextView) findViewById(R.id.error)).setText(R.string.wrong_email_or_password);
+            findViewById(R.id.progressBar).setVisibility(View.INVISIBLE);
+        } else {
+            loginViewModel.loginUser(email, password);
+        }
+    }
 
-        loginViewModel.loginUser(email, password);
+    private boolean validEmail(String email) {
+        return (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
 
-        loginViewModel.userLoginModel.observe(this, userLoginModel -> {
-            // todo
-        });
-        loginViewModel.error.observe(this, error -> {
-            // todo
-        });
+    private boolean validPassword(String password) {
+        return password.length() >= 5;
     }
 }
